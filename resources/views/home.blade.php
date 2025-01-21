7570b15af0c9a6a11965c89a3160b5b4da53a805
Wilson PC <wilsonsamueli33@gmail.com>
2025-01-21 09:19:43 +0100

    Bug Cleared

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -70,7 +70,7 @@
                         {{ Str::replace(' + 1 Lounger', '', $product->specifications['features']['Seating Configuration']) }}
                     </p>
                 </div>
-                <div class="cursor-pointer inline-block mx-auto learn-more-btn mt-2">Learn More {{$appname}} {{$telephone}}</div>
+                <div class="cursor-pointer inline-block mx-auto learn-more-btn mt-2">Learn More</div>
             </a>
         @endforeach
     </section>
